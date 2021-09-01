38a6987c56b97bc3afd7adb85e70952c4dfb8c49
Gustavo Keiji <gugu.keiji.2004@gmail.com>
2021-08-31 22:31:49 -0300

    Cliente Pronto

--- Projeto_Rental/Scripts/1.2-exercicio-locadora-DML.sql
+++ Projeto_Rental/Scripts/1.2-exercicio-locadora-DML.sql
@@ -1,20 +1,35 @@
-USE LOCADORA;
+USE T_Rental;
 GO
 
 INSERT INTO EMPRESA (nomeEmpresa)
-VALUES ('Unidas'), ('Maestro');
+VALUES ('Unidas'), 
+	   ('Maestro');
+GO
 
 INSERT INTO MARCA (nomeMarca)
-VALUES ('VW'), ('GM');
+VALUES ('VW'), 
+	   ('GM');
+GO
 
-INSERT INTO CLIENTE (nomeCliente)
-VALUES ('Jeremias'), ('Augusto');
+INSERT INTO CLIENTE (nomeCliente,sobrenomeCliente)
+VALUES ('Jeremias','Carvalho'), 
+	   ('Augusto','Costa');
+GO
 
 INSERT INTO MODELO (idMarca, nomeModelo)
-VALUES (1,'Polo'), (2,'Celta');
+VALUES (1,'Polo'), 
+	   (2,'Celta');
+GO
 
 INSERT INTO VEICULO (idModelo, idEmpresa, placa)
-VALUES (2,1,'EEE'), (1,2,'AAA');
+VALUES (2,1,'EEE'), 
+	   (1,2,'AAA');
+GO
 
 INSERT INTO ALUGUEL (idVeiculo, idCliente, dataAluguel, dataDevolucao)
-VALUES (1,2,'03/08/2021', '13/08/2021'), (2,1,'04/08/2021', '14/08/2021');
+VALUES (1,2,'03/08/2021', '13/08/2021'), 
+	   (2,1,'04/08/2021', '14/08/2021');
+GO
+
+--UPDATE CLIENTE SET nomeCliente = 'nomeCliente', sobrenomeCliente = '@' WHERE idCliente = 1;
+--UPDATE CLIENTE SET nomeCliente = 'Jeremias', sobrenomeCliente = 'Carvalho' WHERE idCliente = 1;
